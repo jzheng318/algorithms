bcb50d4ca25dc4eb18e62bc1475564ffda95e327
Jacqueline Zheng <jzheng318@gmail.com>
2020-01-21 11:23:44 -0500

    fix: clicking start game begins your game, and clicking restart game resets board and starts new game

--- Challenges/minesweeper/src/components/grid.js
+++ Challenges/minesweeper/src/components/grid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBoard, plantBombs, countNeighbors } from '../functions';
+import { createBoard, plantBombs, updateGrid, lost } from '../functions';
 
 export default class Grid extends React.Component {
   constructor(props) {
@@ -18,45 +18,59 @@ export default class Grid extends React.Component {
     this.setState({
       endGrid: plantBombs(this.state.endGrid, Math.floor(this.size * 1.25)),
     });
-    console.log(this.state);
-  }
-  restartGame() {
-    this.setState({
-      endGrid: createBoard(this.size),
-    });
+    console.log('NEW GAME');
     console.log(this.state);
   }
 
-  updateCell(row, col, val) {
+  async restartGame() {
+    await this.setState({
+      startGrid: createBoard(this.size),
+      endGrid: createBoard(this.size),
+    });
+    console.log(this.state);
+    this.startGame();
+  }
+
+  updateCell(row, col) {
     console.log(`row: ${row}, col index: ${col}`);
-    this.setState({
-        if (val)
-    })
+    let value = this.state.endGrid[row][col];
+    if (value === '💣') {
+      console.log('this is not a number!');
+      this.setState({
+        startGrid: lost(this.state.startGrid),
+      });
+    } else {
+      this.setState({
+        startGrid: updateGrid(this.state.startGrid, row, col, value),
+      });
+    }
   }
 
   render() {
     return (
       <div id="graph">
-        <table>
-          <tbody>
-            {this.state.endGrid.map((row, rowIdx) => (
-              <tr key={rowIdx} row={row} rowidx={rowIdx}>
-                {row.map((col, colIdx) => (
-                  <td
-                    key={colIdx}
-                    value={col}
-                    onClick={() => this.updateCell(rowIdx, colIdx, col)}
-                    colidx={colIdx}
-                  >
-                    {col === 0 ? '' : col}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
         <button onClick={this.startGame}>Start Game</button>
         <button onClick={this.restartGame}>Restart Game</button>
+        <div>
+          <table>
+            <tbody>
+              {this.state.startGrid.map((row, rowIdx) => (
+                <tr key={rowIdx} row={row} rowidx={rowIdx}>
+                  {row.map((col, colIdx) => (
+                    <td
+                      key={colIdx}
+                      value={col}
+                      onClick={() => this.updateCell(rowIdx, colIdx)}
+                      colidx={colIdx}
+                    >
+                      {col === 0 ? '' : col}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
